173d4650481afd51685fbfcff58fd6b264bc6c6c
Edi Permadi <edipermadi@gmail.com>
2023-08-05 15:30:01 +0800

    atom echo bluetooth sink

--- atom_echo_bt_speaker/atom_echo_bt_speaker.ino
+++ atom_echo_bt_speaker/atom_echo_bt_speaker.ino
@@ -14,11 +14,28 @@ LiteLED led(LED_TYPE, LED_TYPE_IS_RGBW);
 BluetoothA2DPSink a2dp_sink;
 
 static const crgb_t L_RED = 0xff0000;
+static const crgb_t L_ORANGE = 0xffa500;
 static const crgb_t L_GREEN = 0x00ff00;
 static const crgb_t L_BLUE = 0x0000ff;
 static const crgb_t L_WHITE = 0xe0e0e0;
 
+static void on_audio_state_changed(esp_a2d_audio_state_t state, void *arg) {
+  switch (state) {
+    case ESP_A2D_AUDIO_STATE_REMOTE_SUSPEND:
+      led.setPixel(0, L_ORANGE, 1);
+      break;
+    case ESP_A2D_AUDIO_STATE_STOPPED:
+      led.setPixel(0, L_RED, 1);
+      break;
+    default:
+      led.setPixel(0, L_GREEN, 1);
+      break;
+  }
+}
+
 void setup() {
+  Serial.begin(115200);
+
   i2s_pin_config_t my_pin_config = {
     .bck_io_num = 19,
     .ws_io_num = 33,
@@ -26,6 +43,8 @@ void setup() {
     .data_in_num = I2S_PIN_NO_CHANGE
   };
   a2dp_sink.set_pin_config(my_pin_config);
+  a2dp_sink.set_mono_downmix(true);
+  a2dp_sink.set_on_audio_state_changed(on_audio_state_changed);
   a2dp_sink.start("Atom Echo");
 
   led.begin(LED_GPIO, 1);
@@ -36,26 +55,22 @@ void setup() {
 
 bool playing = true;
 bool pressed = false;
+
 void loop() {
-  if (a2dp_sink.is_connected()) {
-    led.setPixel(0, L_GREEN, 1);
-    bool status = digitalRead(BUTTON_PIN) == 0;
-    if (pressed != status) {
-      pressed = status;
+  bool status = digitalRead(BUTTON_PIN) == 0;
+  if (pressed != status) {
+    pressed = status;
 
-      if (status) {
-        if (playing) {
-          a2dp_sink.pause();
-        } else {
-          a2dp_sink.play();
-        }
-
-        playing = !playing;
-        delay(1000);
+    if (status) {
+      if (playing) {
+        a2dp_sink.pause();
+      } else {
+        a2dp_sink.play();
       }
+
+      playing = !playing;
+      delay(1000);
     }
-  } else {
-    led.setPixel(0, L_RED, 1);
   }
 
   delay(100);
